84dcf06810959b195dba8360321663924249bd10
AlvaroL <lavado.mejia.alvaro.elias@gmail.com>
2026-06-29 00:18:20 -0500

    feat: implementar la clase abstracta Laptop con sus atributos y sus metodos

--- src/model/Laptop.java
+++ src/model/Laptop.java
@@ -1,4 +1,19 @@
 package model;
 
-public class Laptop {
+public abstract class Laptop{
+    protected String tamaño;
+    protected String tipoProcesador;
+    protected String añoFabricacion;
+    protected String modelo;
+    protected String marca;
+
+    public abstract void ejecutarPrueba();
+
+    @Override
+    public String toString(){
+        return "Marca y Modelo: " +marca+" "+modelo
+                +"\nAño de Fabricacion: " +añoFabricacion
+                +"\nProcesador: "+tipoProcesador
+                +"\nTamaño de la Pantalla: "+tamaño;
+    }
 }
